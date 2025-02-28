4ec75a04b4cd77205f5488da3aa0448be0cdfe91
pado8 <floe9235@gmail.com>
2025-02-28 18:02:24 +0900

    헤더 기본 모양 추가 및 favicon 추가

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
+// const lenis = new Lenis({
+//     duration: 1.2,
+//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+// });
 
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+// function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
 
-gsap.fromTo(".text", { opacity: 0, y: 50 }, {
-    opacity: 1, y: 0, scrollTrigger: {
-        trigger: ".text",
-        start: "top 80%",
-        end: "top 50%",
-        scrub: true
-    }
-});
+// gsap.fromTo(".text", { opacity: 0, y: 50 }, {
+//     opacity: 1, y: 0, scrollTrigger: {
+//         trigger: ".text",
+//         start: "top 80%",
+//         end: "top 50%",
+//         scrub: true
+//     }
+// });
